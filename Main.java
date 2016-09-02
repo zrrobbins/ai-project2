10863ee5ece220a591f5bd3edde0d0187f894a1c
QSSR <phraspe@wpi.edu>
2016-09-02 14:04:32 -0400

    Started Work on the FIle Parsing
    Added FileParse method to parse files for the information we need

--- Main.java
+++ Main.java
@@ -4,12 +4,26 @@
  * CS4341: Project 1
  * Group: Zachary Robbins, Kyle McCormick, Elijah Gonzalez, Peter Raspe
  */
+Boolean searchMethod = new Boolean();
 
 public class Main {
 	public static void main(String[] args){
 		// file reading + parsing
 		//	return a search object
+		/** File Parse
+		 * @param args the information from the command line
+		 * @return a search object parsed from the file 
+		 */
 
+		Search fileParse(String[] args){
+			FileInputStream stream = new FileInputStream();
+			stream.split("\n");
+			for( int i = 0, i < args.length(), i++){
+
+			}
+
+			
+		}
 		// choose a search method
 
 		// give output in correct format 
