cdbad81b3d690e14d14a2f10c7a8594f612f2542
Kyle McCormick <kylemccor@gmail.com>
2016-09-12 21:33:05 -0400

    Function skeletons

--- GeneticSearch.java
+++ GeneticSearch.java
@@ -4,8 +4,12 @@
  * CS4341: Project 2
  * Group: Zachary Robbins, Kyle McCormick, Elijah Gonzalez, Peter Raspe
  */
-import java.util.Stack;
-import java.util.Arrays;
+
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List; 
+import java.util.PriorityQueue;
+import java.util.Random;
 
 /**
  * Implementation of Iterative Deepening Search.
@@ -13,6 +17,7 @@ import java.util.Arrays;
 public class GeneticSearch extends Search {
 
 	int nodesExpanded = 0;
+	final int INIT_POPULATION_SIZE = 10;
 
 	public GeneticSearch(double startValue, double targetValue, double timeLimit, Operation[] operations) {
 		super(startValue,  targetValue, timeLimit, operations);
@@ -32,19 +37,55 @@ public class GeneticSearch extends Search {
 		6.) PROFIT
 	 */
 
+	private List<Operation> generateNewOrganism() {
+		return null; // TODO
+	}
 
+	private List<Operation> reproduce(List<Operation> mother, List<Operation> father) {
+		return null; // TODO
+	}
 
-
+	private void mutate(List<Operation> organism) {
+		// TODO
+	}
 
 	/**
 	 * Perform the search.
 	 */
 	@Override
 	public void performSearch() {
-		System.out.println("LOL");
-
+		PriorityQueue<List<Operation>> population = new PriorityQueue<List<Operation>>(
+			64, new OrganismComparator(this.startValue, this.targetValue)
+		);
+		for (int i = 0; i < INIT_POPULATION_SIZE; i++) {
+			population.add(this.generateNewOrganism());
+		}
+		// TODO: finish
 	}
 
-	
+	private static class OrganismComparator implements Comparator<List<Operation>> {
+		
+		public final double startValue, targetValue;
+
+		public OrganismComparator(double startValue, double targetValue) {
+			this.startValue = startValue;
+			this.targetValue = targetValue;
+		}
+
+		@Override
+		public int compare(List<Operation> a, List<Operation> b) {
+			double aVal = startValue;
+			for (Operation op : a) {
+				aVal = op.applyTo(aVal);
+			}
+			double bVal = startValue;
+			for (Operation op : b) {
+				bVal = op.applyTo(bVal);
+			}
+			double aDiff = Math.abs(aVal - this.targetValue);
+			double bDiff = Math.abs(bVal - this.targetValue);
+			return aDiff == bDiff ? 0  :
+			       aDiff <  bDiff ? -1 : 1;
+		}	
+	}
 }
-	
